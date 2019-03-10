50dfaf42a2b36efe64c128e62c562eeabf682491
haohonghh <haohong278636029@vip.qq.com>
2019-03-10 14:53:53 +0800

    chongtu

--- src/main/java/com/boot/srimplespringboot/git22/Aedg.java
+++ src/main/java/com/boot/srimplespringboot/git22/Aedg.java
@@ -2,6 +2,6 @@ package com.boot.srimplespringboot.git22;
 
 public class Aedg {
     public static void main(String[] args) {
-        System.out.println("dsadahjhkhjkjjksdsadsaasd");
+        System.out.println("dsadahjhkhjkjjknnnsdsadsaasd");
     }
 }
